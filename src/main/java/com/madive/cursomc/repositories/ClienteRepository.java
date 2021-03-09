ca85251a3b1e7ba8ba418f15b6505b9ac1ad4005
Marco Di Veroli <madive60@gmail.com>
2021-03-09 18:33:57 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/madive/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.madive.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.madive.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
